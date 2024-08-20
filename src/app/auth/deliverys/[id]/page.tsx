c43472035e5f493288f3e5548df29b73fb58bdd9
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-08-20 20:39:21 -0300

    chore: page structures

--- src/app/auth/deliverys/[id]/page.tsx
+++ src/app/auth/deliverys/[id]/page.tsx
@@ -1,9 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     <h1>Pagina de login e registro</h1>
+     <h1>Pagina de delivery por id</h1>
     </main>
   );
 }
