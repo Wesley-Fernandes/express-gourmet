bb6a09e43a3dc2420f4bffb9de3c4529655bf37a
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-08-20 22:04:16 -0300

    feat: navbar component

---
+++ src/components/Navbar/Links.tsx
@@ -0,0 +1,18 @@
+import { links } from '@/constants/links'
+import React from 'react'
+import Jumper from './Jumper'
+import { Logout } from './Logout'
+
+export function Links() {
+  return (
+    <div className="flex flex-1 flex-col h-[calc(100vh-5.95rem)]">
+      <ul className='flex flex-1 flex-col'>
+        {
+          links.logged.map((i) => (
+              <Jumper key={i.title} to={i.to}>{i.title}</Jumper>
+          ))}
+      </ul>
+      <Logout/>
+    </div>
+  )
+}
