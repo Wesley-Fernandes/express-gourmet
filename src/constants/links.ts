cab105be66dcb34d6c11491f440c32e684bdf140
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-08-20 22:03:38 -0300

    chore: links constants

---
+++ src/constants/links.ts
@@ -0,0 +1,14 @@
+export const links = {
+    logged: [
+        { title: 'Inicio', to: '/' },
+        { title: 'Deliverys', to: '/auth/deliverys' },
+        { title: 'Procurar', to: '/auth/search' },
+        { title: 'Pedidos', to: '/auth/orders' },
+        { title: 'Configurações', to: '/auth/settings' },
+    ],
+    notLogged: [
+        { title: 'Home', to: '/' },
+        { title: 'Deliverys', to: '/auth/deliverys' },
+        { title: 'Procurar', to: '/auth/search' },
+    ],
+}
