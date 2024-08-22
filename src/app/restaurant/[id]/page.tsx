ce4c933f8634349046b60db6e4bfcbf6842aae7d
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-08-22 03:35:23 -0300

    fix: get enemy by id

--- src/app/restaurant/[id]/page.tsx
+++ src/app/restaurant/[id]/page.tsx
@@ -1,30 +1,36 @@
 /* eslint-disable @next/next/no-img-element */
-import { Complement } from "@/components/Complement";
-import Item from "@/components/Item";
-import CONSTANTS from "@/constants/constants";
+"use client"
+import { getRestaurantFullData } from "@/api/food";
 import { pageProps } from "@/types/page";
+import { useQuery } from "@tanstack/react-query";
 import { Headset, Soup } from "lucide-react";
-
-export default function page({params}:pageProps) {
-  const delivery = CONSTANTS.delivery.filter((food) => food.id === params.id)[0];
-
-  if(!delivery) return <h1>Nenhum delivery encontrada</h1>;
-
-  return (
+import LoadingPage from "@/components/LoadingPage";
+import NotExist from "@/components/NotExist";
+import ErrorPage from "@/components/ErrorPage";
+import Item from "@/components/Item";
+export default function RestaurantById({params}:pageProps) {
+ const {data, isPending, error} = useQuery({
+  queryKey: ['restaurant'],
+  queryFn: async () => await getRestaurantFullData(params.id)
+ });
+ if(!data && !isPending && !error && data==null) return <NotExist/>;
+ if(!data && isPending) return <LoadingPage/>;
+ if(error) return <ErrorPage error={error.message}/>;
+ if(data) return (
     <main className="flex min-h-screen">
      <section className="sm:container w-full flex flex-col">
       <header className="h-48 w-full border-b">
-          <img src={delivery.thumbnail}  alt="background" className="h-full w-full object-cover"/>
+          <img src={data.restaurant.thumbnail}  alt="background" className="h-full w-full object-cover"/>
           <div className="w-full flex items-center justify-around">
             <div className="absolute top-48 flex items-center gap-4">
               <div className="h-28 w-32 bg-white border-2 p-1">
-                <img src={delivery.icon} alt={delivery.name} className="h-full w-full object-fill"/>
+                <img src={data.restaurant.icon} alt={data.restaurant.name} className="h-full w-full object-fill"/>
               </div>
               <div className="flex flex-col gap-2">
-                <h1 className=" mt-12 py-2 text-base text-center font-black text-black rounded-xl uppercase">{delivery.name}</h1>
+                <h1 className=" mt-12 py-2 text-base text-center font-black text-black rounded-xl uppercase">{data.restaurant.name}</h1>
                 <span className="-mt-4 flex items-center gap-1 text-xs">
                   <Headset strokeWidth={1} size={14}/>
-                  {delivery.cellphone}
+                  {data.restaurant.cellphone}
                 </span>
               </div>
             </div>
@@ -34,7 +40,7 @@ export default function page({params}:pageProps) {
         <h1 className="font-bold pl-1 mb-1 text-2xl flex items-center gap-1"><Soup strokeWidth={1}/>Catalogo</h1>
         <ul className="px-1 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
           {
-            delivery.foods.map((food) => {
+            data.foods.map((food) => {
               return (<Item {...food} key={food.id}/>)
             })
           }
