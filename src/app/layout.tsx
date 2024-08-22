fd7e9c3ba003ecc195a50a96517210d7f9ca0884
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-08-22 00:37:58 -0300

    chore: tanstack query config

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import { Toaster } from "sonner";
 import Navbar from "@/components/Navbar";
 import "./globals.css";
+import QueryProvider from "@/components/providers/QueryProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar/>
-        {children}
-        <Toaster/>
+        <QueryProvider>
+          <Navbar/>
+          {children}
+          <Toaster/>
+        </QueryProvider>
       </body>
     </html>
   );
---
+++ src/components/providers/QueryProvider.tsx
@@ -0,0 +1,18 @@
+import { QueryClient, QueryClientProvider, QueryCache} from '@tanstack/react-query'
+import { ReactNode } from 'react'
+
+interface Props{
+  children:ReactNode;
+}
+export default function QueryProvider({children}:Props) {
+    const client = new QueryClient({
+        defaultOptions: {
+          queries: {
+            retryDelay: (attemptIndex) => Math.min(1000 * 2 ** attemptIndex, 30000),
+          },
+        },
+      })
+  return (
+    <QueryClientProvider client={client}>{children}</QueryClientProvider>
+  )
+}
