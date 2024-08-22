97276bdd901efae5a862f647523c0039cbb48c01
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-08-21 23:19:31 -0300

    feat: update delivery initial

--- src/app/deliverys/page.tsx
+++ src/app/deliverys/page.tsx
@@ -2,18 +2,19 @@
 import Item from "@/components/Item";
 import CONSTANTS from "@/constants/constants";
 
-export default function page() {
+export default function FoodSearch() {
   
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-1">
-      <section className=" container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pt-6 gap-2 flex-col w-full">
+    <main className="flex min-h-screen flex-col items-center justify-between p-2">
+      
+      <ul className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pt-6 gap-2 flex-col w-full">
         {
           CONSTANTS.deliverys.map((delivery) => {
             return (
               <Item {...delivery} key={delivery.id}/>
             )}
         )}
-      </section>
+      </ul>
     </main>
   );
 }
