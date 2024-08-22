a045f777557e02c1d7e9f6701804fed2660fa8ec
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-08-21 23:19:07 -0300

    feat: search food by name

--- src/app/food/page.tsx
+++ src/app/food/page.tsx
@@ -1,9 +1,75 @@
-import React from 'react'
+"use client"
+import Item from '@/components/Item'
+import CONSTANTS from '@/constants/constants'
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { LoaderCircle, Search } from 'lucide-react'
+import { FoodInterface } from '@/types/food'
+import { FormEvent, useState } from 'react'
 
-export default function page() {
+export default function FoodSearch() {
+  const [query, setQuery] = useState<FoodInterface[]>([]);
+  const [loading, setLoading] = useState(false);
+  const findData = (e:FormEvent) => {
+    setLoading(true);
+    e.preventDefault();
+
+
+    const target = e.target as typeof e.target & {
+      name: {value: string},
+    }
+
+    const name = target.name.value.toLowerCase();
+
+    if(!name){
+      setQuery([]);
+      setLoading(false);
+      return;
+    }
+    const filteredData = CONSTANTS.foods.filter((food) => food.name.toLowerCase().trim().includes(name));
+    setTimeout(()=>{
+      setQuery(filteredData);
+      setLoading(false);
+      return;
+    }, 3000)
+    return;
+  }
   return (
-    <main className='h-screen'>
-      <h1>Pesquisar comida</h1>
+    <main className='h-[calc(100vh-3.5rem)] min-h-[calc(100vh-3.5rem)] flex flex-1'>
+     <section className='p-4 flex flex-col flex-1 gap-4 container'>
+        <form className="flex items-center gap-2" onSubmit={findData}>
+          <Input type='text' name='name' placeholder='Procurar um item...'/> <Button className='flex items-center gap-1'>Procurar <Search strokeWidth={1} size={15}/></Button>
+        </form>
+        {
+          loading && query.length === 0 && (
+            <div className='flex flex-1 justify-center items-center'>
+              <LoaderCircle strokeWidth={1} size={40} className='animate-spin text-red-600'/>
+            </div>
+          )
+        }
+        {
+          !loading && query.length > 0 && (
+            <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pt-6 gap-2 flex-col w-full">
+              {
+                query.map((delivery) => {
+                  return (
+                    <Item {...delivery} key={delivery.id}/>
+                  )}
+                )
+              }
+          </ul>
+          )
+        }
+
+         {
+          !loading && query.length === 0 && (
+            <div className='flex flex-1 justify-center items-center'>
+              <span>Sem dados para amostrar.</span>
+            </div>
+          )
+        }
+
+     </section>
     </main>
   )
 }
