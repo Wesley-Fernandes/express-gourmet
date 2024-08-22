17d0d92cd619f3363779bd3842a7f9a536b21534
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-08-22 02:45:43 -0300

    feat: not exist component

---
+++ src/components/NotExist.tsx
@@ -0,0 +1,22 @@
+"use client"
+import { ArrowLeft } from 'lucide-react';
+import { Button } from './ui/button'
+import { useRouter } from 'next/navigation'
+
+export default function NotExist() {
+    const {back} = useRouter();
+  return (
+    <section className='flex h-screen flex-col justify-center items-center'>
+        <div className='w-80 p-3'>
+            <Button className='-ml-3 flex gap-1 items-center' onClick={back} variant="outline" >
+                <ArrowLeft strokeWidth={1}/>
+                Retornar
+            </Button>
+        </div>
+        <div className='w-80 border p-4 flex flex-col bg-primary-foreground'>
+            <h1 className='text-2xl font-black'>404</h1>
+            <p>Os dados solicitados não existem.</p>
+        </div>
+    </section>
+  )
+}
