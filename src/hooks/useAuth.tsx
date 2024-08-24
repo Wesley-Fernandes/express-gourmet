d8d0dbca3805c9dc7d850b7a3fe5e794c9d04e9f
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-08-24 02:35:41 -0300

    fix: update user schema

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,13 +1,41 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 "use client"
-import { auth } from '@/services/firebase';
+import { auth } from '@/services/firebase-client';
 import { onAuthStateChanged, User } from 'firebase/auth';
 import { useEffect, useState } from 'react'
 
+interface CustomUser extends User {
+    reloadUserInfo:{
+            customAttributes: {
+                ['admin']?: boolean
+    };
+}
+}
 export function useAuth() {
-    const [user, setUser] = useState<User|null| false>(false);
+    const [user, setUser] = useState<CustomUser|null| false>(false);
     useEffect(()=>{
-        return onAuthStateChanged(auth,(e)=>setUser(e));
+
+        return onAuthStateChanged(auth,(e)=>{
+            if(e==null){
+                setUser(null);
+                return;
+            }
+            const USER = e as CustomUser;
+            const exist = USER.reloadUserInfo.customAttributes ? true : false;
+            let admin: boolean = false;
+
+            if (exist){
+                const custom = JSON.parse(JSON.stringify(USER.reloadUserInfo.customAttributes) || '{}');
+                admin = JSON.parse(custom)["admin"];
+            }else{
+                admin = false;
+            }
+           
+            const NEW_USER = {...USER, reloadUserInfo: {
+                customAttributes: {admin}
+            }}
+            setUser(NEW_USER)
+        });
     }, [])
 
     return user;
