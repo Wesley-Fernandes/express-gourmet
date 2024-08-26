8d84c5bb12846748c2de6536b3db469215b3464d
Wesley Fernandes <wesleyintelligence@protonmail.com>
2024-08-26 03:43:18 -0300

    chore: update image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        domains: ['client-assets.anota.ai', "images.tcdn.com.br"],
+    }
 };
 
 export default nextConfig;
